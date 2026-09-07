a565a8ade65425e265766f3367a97a7fe55c2eca
eshaswamy <eshaswamy@icloud.com>
2026-09-07 16:55:14 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.STUDY_CONFIG = {
   // Deploy the included Google Apps Script as a Web App, then paste its /exec URL here.
-  API_URL: "https://script.google.com/macros/s/AKfycbwGZtKH5zen-QtK6gu59L1j8_X22iz9gTOpYcLI2xj0Inhgf6f6iGaY8P3LK1q-s58/exec",
+  API_URL: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
   REQUIRE_PERMISSION_CODE: true,
 
   // Task parameters. Pilot before changing.
